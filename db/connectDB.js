7d57e462e525b24a35f3dded6b96dd7f9712c7dc
MamunCh0wdhury <kabir.chowdhury1990@gmail.com>
2025-02-03 02:43:44 +0530

    Db Connection Modified

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,3 +1,3 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize(process.env.DB_URL,{  dialect: 'postgres', });
 module.exports = sequelize;
